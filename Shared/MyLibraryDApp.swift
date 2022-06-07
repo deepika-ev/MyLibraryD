5441998ddc298d067202bae847a23bbf947b0857
deepika-ev <deepikaev4444@gmail.com>
2022-06-07 02:18:45 -0700

    Initial Commit

---
+++ Shared/MyLibraryDApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLibraryDApp.swift
+//  Shared
+//
+//  Created by Capgemini_DA230 on 6/7/22.
+//
+
+import SwiftUI
+
+@main
+struct MyLibraryDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
